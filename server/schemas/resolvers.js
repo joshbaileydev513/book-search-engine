7029daf65104aabf938fb0e1ef26c1b4f6ebdcff
joshbaileydev513 <joshbaileydev513@gmail.com>
2023-07-03 15:43:54 -0400

    wrote query and setup for the mutation section

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,35 @@
 const { User } = require('../models');
 const { Query } = require('mongoose');
 const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
 
+const resolvers = {
+    // Write Query code for single user
+    Query: {
+        user: async (parent, args, context) => {  
+            return await User.findOne({ _id: context.user._id }); 
+          },
+    },
+    // write mutation code with an addUser/login/saveBook and deleteBook
+    Mutation: {
+        addUser: 
 
-// Write Query code for single user
+        }
 
-// write mutation code with an addUser/login/saveBook and deleteBook
+        login: 
+        
+        }
+
+        saveBook: 
+        
+        }
+
+        deleteBook: 
+        
+        }
+    }
+};
+
 
 // export file here
 module.exports = resolvers;
