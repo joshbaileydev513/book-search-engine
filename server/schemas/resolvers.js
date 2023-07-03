dd40208fa4520b38018af276a0441a5e51cf387b
joshbaileydev513 <joshbaileydev513@gmail.com>
2023-07-03 16:19:13 -0400

    finished resolvers section

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,20 +12,63 @@ const resolvers = {
     },
     // write mutation code with an addUser/login/saveBook and deleteBook
     Mutation: {
-        addUser: 
+        // addUser mutation
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            
+            if (!user) {
+                return res.status(400).json({ message: 'Error - 404 :/' });
+              }
 
-        }
+            const token = signToken(user);
+            return { token, user };
+        },
 
-        login: 
+        // login mutation
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Sorry - There is no user found with the email address entered');
+            }
+
+            const userPassword = await user.isCorrectPassword(password);
+
+            if (!userPassword) {
+                throw new AuthenticationError('Sorry - There password entered is incorrect');
+            }
+
+            const token = signToken(user);
+            return { token, user };
         
-        }
+        },
 
-        saveBook: 
-        
-        }
+        // saveBook mutation
+        saveBook: async (parent, args, context) => {
+            try {
+                const updateSaveBook = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: args.criteria } },
+                    { new: true, runValidators: true }
+                );
+                return updateSaveBook;
+            } catch (err) {
+                console.log(err);
+                return err;
+            }
+        },
 
-        deleteBook: 
-        
+        // deleteBook mutation
+        deleteBook: async (parent, args, context) => {
+            const deleteSaveBook = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId: args.bookId } } },
+                { new: true }
+            );
+            if (!deleteSaveBook) {
+                return res.status(404).json({ message: "Sorry - Couldn't Delete book with this ID" });
+            }
+            return deleteSaveBook;
         }
     }
 };
